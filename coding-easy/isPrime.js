7ad5b0605ef11b12fe26a236b420548517c889cf
Boris Cherny <boris@performancejs.com>
2017-06-29 23:41:07 -0700

    fix #1

--- coding-easy/isPrime.js
+++ coding-easy/isPrime.js
@@ -1,8 +1,7 @@
 /**
- * This is a lightly optimized version of a naive primality test. We make a few optimizations:
+ * This is a lightly optimized version of a naive primality test. We make a couple of optimizations:
  *
  * - We only need to check up to the square root of our number
- * - We can increment by 2 at a time
  * - We use an iterative solution instead of a recursive one so we are not susceptible to stack overflows
  *
  * Note that much more efficient solutions exist, but are harder to understand. Eg. Google "AKS primality test".
@@ -14,7 +13,7 @@ function isPrime(n) {
   if (n < 2) {
     return false
   }
-  for (let i = 2; i < Math.ceil(Math.sqrt(n)); i += 2) {
+  for (let i = 2; i < Math.ceil(Math.sqrt(n)) + 1; i++) {
     if (n % i === 0) {
       return false
     }
@@ -28,5 +27,8 @@ import { test } from 'ava'
 
 test(t => t.is(isPrime(0), false))
 test(t => t.is(isPrime(1), false))
+test(t => t.is(isPrime(9), false))
 test(t => t.is(isPrime(17), true))
+test(t => t.is(isPrime(25), false))
+test(t => t.is(isPrime(73), true))
 test(t => t.is(isPrime(10000000000000), false))

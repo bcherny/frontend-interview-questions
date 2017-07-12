9baf7c1c500ba8750d08af07457110ed1a80439b
Boris Cherny <boris@performancejs.com>
2017-07-12 12:18:18 -0700

    add caveat to reverse solution (fix #6)

--- coding-easy/reverse.js
+++ coding-easy/reverse.js
@@ -1,3 +1,9 @@
+/**
+* Note: This solution aims for clarity, but does not give correct results when given unicode characters
+* from the supplementary planes (including Chinese characters, emojis, etc.). See discussion here:
+* https://github.com/bcherny/frontend-interview-questions/issues/6.
+*/
+
 /// solution
 
 function reverse(string) {

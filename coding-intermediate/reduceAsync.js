f9a7058fd08c70863e8c5880b9f0561443ade0cf
Boris Cherny <boris@performancejs.com>
2017-07-17 11:25:33 -0700

    update reduceAsync to match test cases in blog post

--- coding-intermediate/reduceAsync.js
+++ coding-intermediate/reduceAsync.js
@@ -14,7 +14,7 @@ import { test } from 'ava'
 test(async t => {
   let a = () => Promise.resolve('a')
   let b = () => Promise.resolve('b')
-  let c = () => Promise.resolve('c')
+  let c = () => new Promise(resolve => setTimeout(() => resolve('c'), 100))
 
   t.deepEqual(
     await reduceAsync([a, b, c], (acc, value) => [...acc, value], []),

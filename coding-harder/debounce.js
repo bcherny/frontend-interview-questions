14ac9ac47c561aaacc3fb39adb35caca1e3614f3
sezgi <sezgi.ulucam@gmail.com>
2021-06-11 18:51:20 -0700

    add simpler solution
    no need to keep track of time diff, since the timer will clear after it runs

--- coding-harder/debounce.js
+++ coding-harder/debounce.js
@@ -1,29 +1,11 @@
 /// solution
 
 function debounce(fn, delay = 0) {
-
-  // keep track of the last call to the debounced function
-  let last = {
-    time: null,
-    timerId: null
-  }
-
-  // return a debounced version of fn
-  return () => {
-    let time = Date.now()
-
-    // if the debounced function was called again before the delay elapsed,
-    // cancel the timer (started in the previous call) that would have called
-    // fn, and start a new timer.
-    if (last.time && (time - last.time) < delay) {
-      clearTimeout(last.timerId)
-    }
-
-    // start a timer to call fn after the given delay
-    last = {
-      time,
-      timerId: setTimeout(fn, delay)
-    }
+  let timer;
+  
+  return function() {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(fn, delay);    
   }
 }
 
